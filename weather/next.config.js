9c9fe6d8b3dc11d1f23cae0dd620ec6c4c1d1173
salahudeen781 <61598552+salahudeen781@users.noreply.github.com>
2024-12-04 14:41:49 -0800

    Update few error
    Update Few error

--- weather/next.config.js
+++ weather/next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrcitmode: true,
-};
-
-module.exports = {
+  reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com", "openweathermap.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "openweathermap.org",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "openweathermap.org",
+        pathname: "/**",
+      },
+    ],
   },
 };
+
+module.exports = nextConfig;
