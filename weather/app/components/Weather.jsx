3faf4adf115cbc19f8aba55723e484751811ea0d
salahudeen781 <61598552+salahudeen781@users.noreply.github.com>
2024-12-04 13:35:00 -0800

    Latest update
    Application does not support Next.js 13, show I m pushing new Latest Next.js

--- weather/app/components/Weather.jsx
+++ weather/app/components/Weather.jsx
@@ -8,10 +8,10 @@ function Weather({ data }) {
       <div>
         <div>
           <Image
-            src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
             alt="/"
-            width="100"
-            height="100"
+            width={100}
+            height={100}
           ></Image>
         </div>
       </div>
